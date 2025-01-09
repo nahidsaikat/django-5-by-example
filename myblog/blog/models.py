d29dfa6ef7e99917f78a8d18f7ee20c01e8f5fab
nahidsaikat <nahidsaikatft40@gmail.com>
2025-01-09 08:41:34 +0600

    Add tagging system to the blog application

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -39,6 +40,8 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+
+    tags = TaggableManager()
 
     class Meta:
         ordering = ["-publish"]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from taggit.models import Tag
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm
@@ -15,8 +16,14 @@ class PostListView(ListView):
     template_name = "blog/post/list.html"
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get("page")
     try:
@@ -28,7 +35,7 @@ def post_list(request):
     return render(
         request,
         "blog/post/list.html",
-        {"posts": posts}
+        {"posts": posts, "tag": tag}
     )
 
 
